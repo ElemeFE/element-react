6de1a3c557513bf71f109a83b11dbcbd13870d24
hopelyn <buaaxigua@gmail.com>
2016-10-27 18:24:31 +0800

    MOD: complete Dialog

--- site/pages/dialog/index.js
+++ site/pages/dialog/index.js
@@ -6,7 +6,6 @@ import './style.scss';
 
 import React from 'react';
 import { Dialog, Layout, Button } from '../../../src';
-import classnames from 'classnames';
 
 const Row = Layout.Row;
 const Col = Layout.Col;
@@ -17,6 +16,8 @@ export default class Playground extends React.Component {
 
     this.state = {
       dialogVisible1: false,
+      dialogVisible2: false,
+      dialogVisible3: false
     };
   }
 
@@ -39,13 +40,68 @@ export default class Playground extends React.Component {
 
           <div className="demo-content demo-dialog">
             <Button type="text" onClick={ () => this.setState({ dialogVisible1: true }) }>点击打开 Dialog</Button>
+            <Dialog title="提示"
+                    size="tiny"
+                    visible={ this.state.dialogVisible1 }
+                    onCancel={ e => this.handleDialogClose(1) }
+            >
+              <Dialog.Body>
+                <span>这是一段信息</span>
+              </Dialog.Body>
+              <Dialog.Footer className="dialog-footer">
+                <Button onClick={ () => this.handleDialogClose(1) }>取 消</Button>
+                <Button type="primary" onClick={ () => this.handleDialogClose(1) }>确 定</Button>
+              </Dialog.Footer>
+            </Dialog>
+          </div>
+        </section>
 
-            <Dialog title="提示" visible={ this.state.dialogVisible1 } size="tiny" handleDialogClose={ () => this.handleDialogClose(1) }>
-              <span>这是一段信息</span>
-              <span slot="footer" class="dialog-footer">
-                <Button click="dialogVisible = false">取 消</Button>
-                <Button type="primary" click="dialogVisible = false">确 定</Button>
-              </span>
+        <section className="demo-section">
+          <div className="demo-header">
+            <h3>自定义内容</h3>
+            <p>Dialog 组件的内容可以是任意的，甚至可以是表格或表单，下面是应用了 Element Table 和 Form 组件的两个样例。</p>
+          </div>
+
+          <div className="demo-content demo-dialog">
+            <Button type="text" onClick={ () => this.setState({ dialogVisible2: true }) } type="text">打开嵌套表格的 Dialog</Button>
+            <Dialog title="收货地址"
+                    visible={ this.state.dialogVisible2 }
+                    onCancel={ (e) => this.handleDialogClose(2) }
+            >
+              <Dialog.Body>
+                TODO: 待 Table 实现
+                <el-table data="gridData">
+                  <el-table-column property="date" label="日期" width="150"></el-table-column>
+                  <el-table-column property="name" label="姓名" width="200"></el-table-column>
+                  <el-table-column property="address" label="地址"></el-table-column>
+                </el-table>
+              </Dialog.Body>
+            </Dialog>
+
+            <Button type="text" onClick={ () => this.setState({ dialogVisible3: true }) } type="text">打开嵌套表单的 Dialog</Button>
+            <Dialog title="收货地址"
+                    visible={ this.state.dialogVisible3 }
+                    onCancel={ (e) => this.handleDialogClose(3) }
+            >
+              <Dialog.Body>
+                TODO: 待 Form 实现
+                <el-form model="form">
+                  <el-form-item label="活动名称" label-width="formLabelWidth">
+                    <el-input v-model="form.name" auto-complete="off"></el-input>
+                  </el-form-item>
+                  <el-form-item label="活动区域" label-width="formLabelWidth">
+                    <el-select v-model="form.region" placeholder="请选择活动区域">
+                      <el-option label="区域一" value="shanghai"></el-option>
+                      <el-option label="区域二" value="beijing"></el-option>
+                    </el-select>
+                  </el-form-item>
+                </el-form>
+              </Dialog.Body>
+
+              <Dialog.Footer className="dialog-footer">
+                <Button onClick={ () => this.handleDialogClose(3) }>取 消</Button>
+                <Button type="primary" onClick={ () => this.handleDialogClose(3) }>确 定</Button>
+              </Dialog.Footer>
             </Dialog>
           </div>
         </section>
--- src/dialog/index.js
+++ src/dialog/index.js
@@ -5,60 +5,126 @@
 import React, { PropTypes } from 'react';
 import { Component, View } from '../../libs';
 
+const KeyCode = { ESC: 27 };
+
+class Body extends Component {
+  render() {
+    return (
+      <div className="el-dialog__body">
+        { this.props.children }
+      </div>
+    )
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="el-dialog__footer">
+        { this.props.children }
+      </div>
+    )
+  }
+}
+
 export default class Dialog extends Component {
-  close() {
-    return this.props.handleDialogClose();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bodyOverflow: null
+    }
   }
 
-  handleWrapperClick() {
-    return this.close();
+  handleWrapperClick(e) {
+    // if (this.props.closeOnClickModal && e.target.className === 'v-modal') {
+    if (this.props.closeOnClickModal && e.target === e.currentTarget) {
+      this.close(e);
+    }
+  }
+
+  close(e) {
+    this.props.onCancel(e);
+  }
+
+  willOpen(prevProps) {
+    return (!prevProps.visible && this.props.visible);
+  }
+
+  willClose(prevProps) {
+    return (prevProps.visible && !this.props.visible);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.willOpen(prevProps)){
+      this.refs.wrap.focus();
+
+      if (this.props.lockScroll) {
+        if (!this.state.bodyOverflow) {
+          this.setState({
+            bodyOverflow: document.body.style.overflow
+          });
+        }
+        document.body.style.overflow = 'hidden';
+      }
+    }
+
+    if (this.willClose(prevProps) && prevProps.lockScroll) {
+      if (this.props.modal && this.state.bodyOverflow !== 'hidden') {
+        document.body.style.overflow = this.bodyOverflow;
+      }
+    }
+  }
+
+  onKeyDown(e) {
+    if (this.props.closeOnPressEscape && e.keyCode === KeyCode.ESC) {
+      this.close(e);
+    }
   }
 
   render() {
-    // 判断 size 是 tiny/small/large/full 中的一个
-
-    const { visible, title, size, top, modal, customClass, lockScroll, closeOnClickModal, closeOnPressEscape } = this.props;
-
+    const { visible, title, size, top, modal, customClass } = this.props;
     const sizeClass = `el-dialog--${ size }`;
     const style = (size === 'full') ? {} : { 'marginBottom': '50px', 'top': top };
 
-    return (
-      <View show={ visible }>
-        <div className="el-dialog__wrapper" onClick={ () => this.handleWrapperClick() }>
-          <div className={ this.classNames("el-dialog", sizeClass, customClass) } ref="dialog" style={ style }>
-            <div className="el-dialog__header">
-              <span className="el-dialog__title">{ title }</span>
-              <div className="el-dialog__headerbtn">
-                <i className="el-dialog__close el-icon el-icon-close" onClick={ () => this.close() }></i>
-              </div>
-            </div>
-
-            <div className="el-dialog__body" if="rendered"></div>
-
-            <div className="el-dialog__footer" if="$slots.footer"></div>
-          </div>
+    const header = (
+      <div className="el-dialog__header">
+        <span className="el-dialog__title">{ title }</span>
+        <div className="el-dialog__headerbtn">
+          <i className="el-dialog__close el-icon el-icon-close" onClick={ e => this.close(e) }></i>
         </div>
+      </div>
+    );
+
+    const dimmer = (
+      <View show={ modal } transition="v-modal">
+        <div className="v-modal" style={{ zIndex: 1012 }}></div>
       </View>
     );
 
-    // return (
-    //   <View className="el-dialog__wrapper" show={ this.props.visible } onClick="handleWrapperClick">
-    //     <div className={ this.classNames("el-dialog", sizeClass, customClass) } ref="dialog" style={ style }>
-    //       <div className="el-dialog__header">
-    //         <span className="el-dialog__title">{ this.props.title }</span>
-    //         <div className="el-dialog__headerbtn">
-    //           <i className="el-dialog__close el-icon el-icon-close" onClick='close()'></i>
-    //         </div>
-    //       </div>
-    //
-    //       <div className="el-dialog__body" v-if="rendered"><slot></slot></div>
-    //
-    //       <div className="el-dialog__footer" v-if="$slots.footer">
-    //         <slot name="footer"></slot>
-    //       </div>
-    //     </div>
-    //   </View>
-    // )
+    return (
+      <View show={ visible } transition="dialog-fade" key="dialog">
+        <div>
+          <div className="el-dialog__wrapper"
+               tabIndex={ -1 }
+               onKeyDown={ e => this.onKeyDown(e) }
+               onClick={ e => this.handleWrapperClick(e) }
+               ref="wrap"
+               style={{ zIndex: 1013 }}
+          >
+            <div className={ this.classNames("el-dialog", sizeClass, customClass) }
+                 ref="dialog"
+                 style={ style }
+            >
+              { header }
+              { this.props.children }
+            </div>
+          </div>
+
+          { dimmer }
+        </div>
+      </View>
+    );
   }
 }
 
@@ -72,19 +138,17 @@ Dialog.propTypes = {
   // top 值（仅在 size 不为 full 时有效）
   top: PropTypes.string,
   // 控制遮罩层展示
-  modal: PropTypes.bool,// => TODO: 改成 showModal ?
+  modal: PropTypes.bool,
   // Dialog 的自定义类名
-  customClass: PropTypes.string,// TODO: 应该添加数组的支持
+  customClass: PropTypes.string,
   // 是否在 Dialog 出现时将 body 滚动锁定
   lockScroll: PropTypes.bool,
   // 是否可以通过点击 modal 关闭 Dialog
   closeOnClickModal: PropTypes.bool,
   // 是否可以通过按下 ESC 关闭 Dialog
   closeOnPressEscape: PropTypes.bool,
-
-  // TODO: Events
-  // 控制对话框关闭
-  handleDialogClose: PropTypes.func.isRequired
+  // 点击遮罩层或右上角叉或取消按钮的回调
+  onCancel: PropTypes.func.isRequired
 };
 
 Dialog.defaultProps = {
@@ -97,3 +161,6 @@ Dialog.defaultProps = {
   closeOnClickModal: true,
   closeOnPressEscape: true
 };
+
+Dialog.Body = Body;
+Dialog.Footer = Footer;
