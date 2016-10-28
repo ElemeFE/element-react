9db00c4db5c4fff32dc008c8bec8e5d2671f6753
Ma Ning <maning0809@gmail.com>
2016-10-28 18:56:31 +0800

    修改Component引用方式

--- demo/pages/badge/index.js
+++ demo/pages/badge/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Markdown } from '../../../libs';
+import React from 'react';
+import { Component, Markdown } from '../../../libs';
 
 import './style.scss';
 
