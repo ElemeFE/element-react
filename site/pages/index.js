e5e22736a42d6ffb1dd473906eb9f92505361863
高宁 <ning.gao@ele.me>
2016-11-02 18:06:52 +0800

    add InputNumber

--- site/pages/index.js
+++ site/pages/index.js
@@ -27,41 +27,43 @@ import Steps from './steps';
 import Typography from './typography';
 import Color from './color';
 import Breadcrumb from './breadcrumb';
+import InputNumber from './input-number';
 
 // pages是有序的Object, 会影响到左侧的菜单顺序.
 const pages = {
   'Basic': {
-    layout: { title: 'Layout 布局', component: Layout },
-    color: { title: 'Color 色彩', component: Color },
-    typography: { title: 'Typography 字体', component: Typography },
-    icon: { title: 'Icon 图标', component: Icon },
-    button: { title: 'Button 按钮', component: Button },
+    'layout': { title: 'Layout 布局', component: Layout },
+    'color': { title: 'Color 色彩', component: Color },
+    'typography': { title: 'Typography 字体', component: Typography },
+    'icon': { title: 'Icon 图标', component: Icon },
+    'button': { title: 'Button 按钮', component: Button },
   },
   'Form': {
-    radio: { title: 'Radio 单选框', component: Radio },
-    input: { title: 'Input 输入框', component: Input },
+    'radio': { title: 'Radio 单选框', component: Radio },
+    'input': { title: 'Input 输入框', component: Input },
+    'input-number': { title: 'Input Number 计数器', component: InputNumber },
   },
   'Data': {
-    progress: { title: 'Progress 进度条', component: Progress },
-    tree: { title: 'Tree 树形控件', component: Tree },
-    badge: { title: 'Badge 标记', component: Badge },
+    'progress': { title: 'Progress 进度条', component: Progress },
+    'tree': { title: 'Tree 树形控件', component: Tree },
+    'badge': { title: 'Badge 标记', component: Badge },
   },
   'Notice': {
-    alert: { title: 'Alert 警告', component: Alert },
-    loading: { title: 'Loading 加载', component: Loading },
-    message: { title: 'Message 消息提示', component: Message },
-    messageBox: { title: 'Message Box 弹框', component: MessageBox },
-    notification: { title: 'Notification 通知', component: Notification },
+    'alert': { title: 'Alert 警告', component: Alert },
+    'loading': { title: 'Loading 加载', component: Loading },
+    'message': { title: 'Message 消息提示', component: Message },
+    'message-box': { title: 'Message Box 弹框', component: MessageBox },
+    'notification': { title: 'Notification 通知', component: Notification },
   },
   'Nav': {
-    menu: { title: 'NavMenu 导航菜单', component: Menu },
-    breadcrumb: { title: 'Breadcrumb 面包屑', component: Breadcrumb },
-    steps: { title: 'Steps 步骤', component: Steps },
+    'menu': { title: 'NavMenu 导航菜单', component: Menu },
+    'breadcrumb': { title: 'Breadcrumb 面包屑', component: Breadcrumb },
+    'steps': { title: 'Steps 步骤', component: Steps },
   },
   'Others': {
-    dialog: { title: 'Dialog 对话框', component: Dialog },
-    tooltip: { title: 'Tooltip 文字提示', component: Tooltip },
-    card: { title: 'Card 卡片', component: Card }
+    'dialog': { title: 'Dialog 对话框', component: Dialog },
+    'tooltip': { title: 'Tooltip 文字提示', component: Tooltip },
+    'card': { title: 'Card 卡片', component: Card }
   }
 };
 
--- src/input-number/InputNumber.jsx
+++ src/input-number/InputNumber.jsx
@@ -1,10 +1,102 @@
 import React from 'react';
 import { Component, PropTypes } from '../../libs';
 
-export default class InputNumber extends Component {
+import Input from '../input';
+import util from './util';
 
+export default class InputNumber extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: props.value
+    };
+  }
+
+  minDisabled() {
+    return this.state.value - this.props.step < this.props.min;
+  }
+
+  maxDisabled() {
+    return this.state.value + this.props.step > this.state.max;
+  }
+
+  increase() {
+    const { step, max, disabled } = this.props;
+    let { value, inputActive } = this.state;
+
+    if (value + step > max || disabled) return;
+
+    value = util.accAdd(step, value);
+
+    if (this.maxDisabled()) {
+      inputActive = false;
+    }
+
+    this.setState({ value, inputActive });
+  }
+
+  decrease() {
+    const { step, min, disabled } = this.props;
+    let { value, inputActive } = this.state;
+
+    if (value - step < min || disabled) return;
+
+    value = util.accSub(value, step);
+
+    if (this.minDisabled()) {
+      inputActive = false;
+    }
+
+    this.setState({ value, inputActive });
+  }
+
+  activeInput(disabled) {
+    if (!this.props.disabled && !disabled) {
+      this.setState({
+        inputActive: true
+      })
+    }
+  }
+
+  inactiveInput(disabled) {
+    if (!this.props.disabled && !disabled) {
+      this.setState({
+        inputActive: false
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.classNames('el-input-number', this.props.size && `el-input-number--${this.props.size}`, {
+        'is-disabled': this.props.disabled
+      })}>
+        <Input className={this.classNames({
+          'is-active': this.state.inputActive
+        })} value={this.props.value} disabled={this.props.disabled} size={this.props.size} />
+        <span className={this.classNames('el-input-number__decrease el-icon-minus', {
+          'is-disabled': this.minDisabled()
+        })} onClick={this.decrease.bind(this)} onMouseEnter={this.activeInput.bind(this, this.minDisabled())} onMouseLeave={this.inactiveInput.bind(this, this.minDisabled())} />
+        <span className={this.classNames('el-input-number__increase el-icon-plus', {
+          'is-disabled': this.maxDisabled()
+        })} onClick={this.increase.bind(this)} onMouseEnter={this.activeInput.bind(this, this.maxDisabled())} onMouseLeave={this.inactiveInput.bind(this, this.maxDisabled())} />
+      </div>
+    )
+  }
 }
 
 InputNumber.propTypes = {
+  value: PropTypes.number.isRequired,
+  step: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  max: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  min: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  disabled: PropTypes.bool,
+  size: PropTypes.string
+}
 
+InputNumber.defaultProps = {
+  step: 1,
+  max: Infinity,
+  min: 0
 }
