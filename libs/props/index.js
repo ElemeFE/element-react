a00fade0d743cd2e30a8db7032a192f1a7e477bd
e1emeb0t <ning.gao@ele.me>
2016-11-07 16:56:00 +0800

    Initial Commit

--- libs/props/index.js
+++
@@ -1,6 +0,0 @@
-import { PropTypes } from 'react';
-
-PropTypes.range = require('./range');
-PropTypes.regex = require('./regex');
-
-export default PropTypes;
--- src/checkbox/index.js
+++
@@ -1,6 +0,0 @@
-import Checkbox from './Checkbox';
-import CheckboxGroup from './CheckboxGroup';
-
-Checkbox.Group = CheckboxGroup;
-
-export default Checkbox;
