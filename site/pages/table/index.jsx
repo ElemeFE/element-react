34d9ea5a817596cb0acc386ff933e26a4a7061c4
Matthew Gao <gaoning87@163.com>
2016-11-08 21:48:37 +0800

    fix Table render issue

--- site/pages/table/index.jsx
+++ site/pages/table/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Component, Markdown } from '../../../libs';
+import { Markdown } from '../../../libs';
 import template from '../../docs/zh-CN/table.md';
 
 import './style.scss';
 
-export default class Playground extends Component {
-  ender() {
+export default class Playground extends React.Component {
+  render() {
     return <Markdown context={this} component="Table">{template}</Markdown>
   }
 }
