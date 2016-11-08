849e897a5a179fb8ba2d5cce9a747c9c51d15c3a
e1emeb0t <ning.gao@ele.me>
2016-11-08 11:00:26 +0800

    Move locale from src into libs

--- libs/index.js
+++ libs/index.js
@@ -2,4 +2,5 @@ export { default as Markdown } from './markdown';
 export { default as Transition } from './transition';
 export { default as Component } from './component';
 export { default as PropTypes } from './props';
+export { default as i18n } from './locale';
 export { default as View } from './view';
