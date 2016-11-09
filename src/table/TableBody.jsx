54bf07d3e73aaf835057b0bfc9d260e17197a675
卢景锋 <jingfeng.lu@ele.me>
2016-11-09 18:20:45 +0800

    表格组件功能：带斑马纹表格，带边框表格，带状态表格,固定表头,列拖拽

--- src/table/TableBody.jsx
+++ src/table/TableBody.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
 import { Component, PropTypes } from '../../libs';
+import classnames from 'classnames';
+
+class BodyItem extends Component{
+  constructor(props, context){
+    super(props, context);
+    this.state = {
+      hover: false
+    };
+  } 
+
+  onMouseState(hover){
+    this.setState({
+      hover: hover
+    });
+  }
+
+  render(){
+    const { itemData, columns, rowIndex, rowClassName } = this.props;
+    const rootClassName = classnames({
+      'hover-row': this.state.hover,
+      [rowClassName&&rowClassName(itemData, rowIndex)]: true
+    });
+
+    return (
+      <tr 
+        className={rootClassName}
+        onMouseEnter={(e)=>{this.onMouseState(true)}} 
+        onMouseLeave={e=>this.onMouseState(false)}>
+        {
+          columns.map((col, idx)=>{
+            return (
+              <td
+                key={idx}
+                style={{width: col.width?col.width: ''}}>
+                <div className="cell">{itemData[col.prop]}</div>
+              </td>
+            )
+          })
+        }
+      </tr>
+    )
+  }
+}
+
+BodyItem.propTypes = {
+  columns: PropTypes.array.isRequired,
+  itemData: PropTypes.object.isRequired,
+};
+
 
 export default class TableBody extends  Component{
   render() {
-    const { columns, data, style } = this.props;
+    const { 
+      columns, 
+      data, 
+      style,
+      rowClassName } = this.props;
 
     return (
       <table
@@ -11,22 +64,15 @@ export default class TableBody extends  Component{
         cellPadding={0}
         cellSpacing={0}>
         <tbody>
-          {
+          { 
             data.map((dataItem, dataIdx)=>{
               return (
-                <tr key={dataIdx}>
-                  {
-                    columns.map((col, colIdx)=>{
-                      return (
-                        <td
-                          key={colIdx}
-                          style={{width: col.width?col.width: ''}}>
-                          <div className="cell">{dataItem[col.prop]}</div>
-                        </td>
-                      )
-                    })
-                  }
-                </tr>
+                <BodyItem 
+                  key={dataIdx} 
+                  rowIndex={dataIdx}
+                  rowClassName={rowClassName}
+                  itemData={dataItem} 
+                  columns={columns}/>
               )
             })
           }
@@ -35,6 +81,10 @@ export default class TableBody extends  Component{
     )
   }
 }
+
+TableBody.contextTypes = {
+  $parent: React.PropTypes.object
+};
 
 TableBody.propTypes = {
   columns: PropTypes.array.isRequired,
