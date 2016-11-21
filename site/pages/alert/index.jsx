6450e08892b041b274ba8afcb339df3cebd9d9c6
e1emeb0t <ning.gao@ele.me>
2016-11-21 18:37:56 +0800

    Site localization feature

--- site/pages/alert/index.jsx
+++ site/pages/alert/index.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import { Markdown } from '../../../libs';
-import template from '../../docs/zh-CN/alert.md';
+// import template from '../../docs/zh-CN/alert.md';
 
 import './style.scss';
 
 export default class Playground extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  componentDidMount() {
+    require(['../../docs/zh-CN/alert.md', '../../docs/en-US/alert.md'], this.setTemplate.bind(this))
+  }
+
+  setTemplate(zh_CN, en_US) {
+    const template = {
+      'zh-CN': zh_CN,
+      'en-US': en_US
+    }[navigator.language] || zh_CN;
+
+    this.setState({ template });
+  }
+
+  getTemplate() {
+
+  }
+
   render() {
-    return <Markdown context={this} component="Alert">{template}</Markdown>
+    return <Markdown context={this} component="Alert">{this.state.template}</Markdown>
   }
 }
