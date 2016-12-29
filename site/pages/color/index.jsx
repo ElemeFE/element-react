99f64012d5dd213768337751c99a618790ef73ad
e1emeb0t <ning.gao@ele.me>
2016-12-29 15:54:34 +0800

    Update

--- site/pages/color/index.jsx
+++ site/pages/color/index.jsx
@@ -3,6 +3,10 @@ import Markdown from '../../../libs/markdown';
 import './style.scss';
 
 export default class Color extends Markdown {
+  documentShouldTransform() {
+    return false;
+  }
+  
   document(locale) {
     switch (locale) {
       default:
--- site/pages/typography/index.jsx
+++ site/pages/typography/index.jsx
@@ -3,6 +3,10 @@ import Markdown from '../../../libs/markdown';
 import './style.scss';
 
 export default class Typography extends Markdown {
+  documentShouldTransform() {
+    return false;
+  }
+  
   document(locale) {
     switch (locale) {
       default:
