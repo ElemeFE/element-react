2578d5f975db8835f0cfba393cd9a90dab3ff84c
e1emeb0t <ning.gao@ele.me>
2017-06-21 10:21:34 +0800

    Upgrade webpack to v3

--- build/site/webpack.config.js
+++ build/site/webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
     new HtmlWebpackPlugin({ template: './index.html' })
   ].concat(process.env.TRAVIS_CI ? [] : [
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       mangle: {
         keep_fnames: true
