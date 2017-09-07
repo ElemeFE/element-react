72785d6903c8df033c60ed43ba0fad3b7b7347e3
梁森 <sen.liang@ele.me>
2017-09-07 18:33:16 +0800

    update

--- src/table/TableFooter.jsx
+++ src/table/TableFooter.jsx
@@ -83,7 +83,7 @@ export default class TableFooter extends Component<TableFooterProps> {
               </td>
             ))}
             {!fixed && (
-              <th
+              <td
                 className="gutter"
                 style={{ width: layout.scrollY ? layout.gutterWidth : 0 }}
               />
