75602d6f81dc469320419c8a708ab4c1ed857cdc
梁森 <sen.liang@ele.me>
2017-10-12 17:07:35 +0800

    add Table beta(_Table)

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ export { default as InputNumber } from './input-number';
 export { default as Checkbox } from './checkbox';
 export { default as Slider } from './slider';
 export { default as Table } from './table'
+export { default as _Table } from './_table';
 export { default as Switch } from './switch';
 export { default as Form } from './form';
 export { default as Upload } from './upload';
--- src/table/Types.js
+++ src/table/Types.js
@@ -1,99 +1,39 @@
 // @flow
-export type strOrNum = string | number;
-
 export type Column = {
-  type?: string,
-  columnKey?: string,
-  label?: string,
-  prop?: string,
-  property?: string,
-  width?: number,
-  minWidth?: number,
-  fixed?: true | 'left' | 'right',
-  render?: (row: Object, column: _Column, index: number) => Object,
-  renderHeader?: () => void,
-  sortable: boolean | 'custom',
-  sortMethod?: (a: Object, b: Object) => boolean,
+  label: string,
+  prop: string,
+  property: string,
+  type: string,
+  minWidth: number,
+  width: number,
+  align: string,
+  sortable: boolean,
+  sortMethod: ()=>void,
   resizable: boolean,
-  formatter?: ()=>void,
-  showOverflowTooltip: boolean,
-  align: 'left' | 'center' | 'right',
-  headerAlign: 'left' | 'center' | 'right',
-  className?: string,
-  labelClassName?: string,
-  selectable?: (row: Object, index: number) => boolean,
-  reserveSelection: boolean,
-  filters?: Array<{ text: any, value: any }>,
-  filterPlacement?: string,
-  filterMultiple: boolean,
-  filterMethod?: (value: any, row: Object) => boolean,
-  filteredValue?: Array<strOrNum> | strOrNum,
-  subColumns?: Array<Column>
+  formatter: ()=>void,
+  selectable: boolean,
+  fixed: boolean | string,
+  filterMethod: ()=>void,
+  filters: Array<Object>,
+  expandPannel: (any)=>any,
+  render: ()=>void
 };
 
-export type _Column = {
-  type?: string,
-  columnKey?: string,
-  label?: string,
-  prop?: string,
-  property?: string,
-  width?: number,
-  minWidth?: number,
-  fixed?: true | 'left' | 'right',
-  sortable: boolean | 'custom',
-  sortMethod?: (a: Object, b: Object) => boolean,
-  resizable: boolean,
-  showOverflowTooltip: boolean,
-  align: 'left' | 'center' | 'right',
-  headerAlign: 'left' | 'center' | 'right',
-  className?: string,
-  labelClassName?: string,
-  selectable?: (row: Object, index: number) => boolean,
-  reserveSelection: boolean,
-  filters?: Array<{ text: any, value: any }>,
-  filterPlacement?: string,
-  filterMultiple: boolean,
-  filterMethod?: (value: any, row: Object) => boolean,
-  filteredValue?: Array<strOrNum> | strOrNum,
-  subColumns?: Array<Column>,
-  realWidth: number,
-  render: (row: Object, column: _Column, index: number) => Object,
-  renderHeader: (column: _Column) => Object,
-  filterable: boolean,
-  filterOpened: boolean,
-  rowSpan: number,
-  colSpan: number,
-  level: number,
-  subColumns?: Array<_Column>
-};
-
-export type TableStoreProps = {
-  style?: Object,
-  className?: string,
-  data?: Array<Object>,
-  columns?: Array<Column>,
-  height?: strOrNum,
-  maxHeight?: strOrNum,
+export type TableProps = {
+  columns: Array<Column>,
+  data: Array<Object>,
+  height: number,
+  maxHeight: number,
   stripe: boolean,
   border: boolean,
   fit: boolean,
-  showHeader: boolean,
+  rowClassName: ()=>void,
+  style: Object,
   highlightCurrentRow: boolean,
-  currentRowKey?: strOrNum | Array<strOrNum>,
-  rowClassName?: ((row: Object, index: number) => string) | string,
-  rowStyle: ((row: Object, index: number) => Object) | Object,
-  rowKey: ((row: Object) => strOrNum) | string,
-  emptyText: string,
-  defaultExpandAll: boolean,
-  expandRowKeys?: Array<number | string>,
-  defaultSort?: {
-    prop: string,
-    order?: 'ascending' | 'descending',
-  },
-  tooltipEffect?: 'dark' | 'light',
-  showSummary: boolean,
   sumText: string,
-  summaryMethod?: ({ column: Array<Column>, data: Array<Object> }) => any,
+  emptyText: string,
+  showSummary: boolean,
+  getSummaries: ()=>void,
 
   //Event
   onCurrentChange: ()=>void,
@@ -101,51 +41,116 @@ export type TableStoreProps = {
   onSelectChange: ()=>void
 };
 
-export type TableStoreState = {
-  sortedData: Array<Object>,
+export type TableState = {
+  columns: Array<Column>,
+  _columns: Array<Object>,
+  fixedLeftColumns: Array<Object>,
+  fixedRightColumns: Array<Object>,
   data: Array<Object>,
-  _columns: Array<_Column>,
-  fixedColumns: Array<_Column>,
-  rightFixedColumns: Array<_Column>,
-  originColumns: Array<_Column>,
-  columns: Array<_Column>,
-  isComplex: boolean,
-  defaultExpandAll: boolean,
+  sortList: null | Array<Object>,
+  filterList: null | Array<Object>,
+
+  bodyWidth: '' | number,
+  bodyHeight: '' | number,
+  headerHeight: '' | number,
+  realTableHeaderHeight: '' | number,
+  realTableHeight: '' | number,
+  realTableWidth: '' | number,
+  resizeProxyVisible: boolean,
+
+  scrollY: boolean,
+  scrollX: boolean
 };
 
-export type TableLayoutProps = TableStoreProps & { store: TableStoreState, renderExpanded?: (row: Object, rowIndex: number) => Object };
-
-export type TableLayoutState = {
-  height?: strOrNum,
-  gutterWidth: number,
-  tableHeight?: number,
-  headerHeight?: number,
-  bodyHeight?: number,
-  footerHeight?: number,
-  fixedBodyHeight?: number,
-  viewportHeight?: number,
-  scrollX?: boolean,
-  scrollY?: boolean,
+export type DefaultTableProps = {
+  columns: Array<Column>,
+  data: Array<Object>,
+  stripe: boolean,
+  border: boolean,
+  fit: boolean,
+  highlightCurrentRow: boolean
 };
 
-export type TableProps = TableLayoutProps & { layout: TableLayoutState };
 
-export type TableState = {};
+export type TableHeaderProps = {
+  columns: Array<Column>,
+  fixed: string,
+  border: string,
+  style: Object,
+  isScrollY: boolean,
+  flattenColumns: Object
+};
 
-export type TableHeaderProps = TableProps & { fixed: true | 'left' | 'right' };
-export type TableBodyProps = TableHeaderProps;
-export type TableFooterProps = TableHeaderProps;
+export type TableHeaderState = {
+  allChecked: boolean,
+  dragging: boolean,
+  dragState: Object,
+  sortStatus: number,
+  sortPropertyName: string,
+  filterParams: Object
+};
 
+export type TableBodyProps = {
+  columns: Array<Column>,
+  data: Array<Object>,
+  rowClassName: ()=>void,
+  highlightCurrentRow: boolean,
+  style: Object,
+  fixed: string,
+  flattenColumns: Object
+};
+
+export type TableBodyState = {
+  highlightRows: Array<Object>,
+  selected: Array<Object>
+};
+
+export type TableBodyItemProps = {
+  columns: Array<Column>,
+  itemData: Object,
+  fixed: string,
+  onSelected: (boolean, any)=>void,
+  selected: boolean,
+  isHiglight: boolean,
+  rowIndex: number,
+  rowClassName: (any, any)=>string,
+  leafColumns: Array<Object>
+};
+
+export type TableBodyItemState = {
+  hover: boolean,
+  expand: boolean
+};
+
+export type TableFooterState = {
+  dataList: Array<mixed>
+};
+
+export type TableFooterProps = {
+  leafColumns: Array<Object>,
+  sumText: string,
+  data: Array<Object>,
+  getSummaries: (any, any) => mixed
+};
 
 export type FilterProps = {
-  visible: boolean,
-  multiple: boolean,
+  defaultCondi: null | Array<Object>,
   filters: Array<Object>,
-  filteredValue: Array<any>,
-  onFilterChange: (value: any) => void,
-  toggleFilter: () => void
-}
+  onFilter: (any)=>void,
+  onClose: ()=>void,
+  visible: boolean,
+  position: Object,
+  popper: any,
+  ower: Object
+};
 
 export type FilterState = {
-  filteredValue: Array<any>,
+  visible: boolean,
+  defaultStyle: Object,
+  checked: Array<Object>
+};
+
+export type FilterDefaultProps = {
+  filters: Array<Object>,
+  onFilter: ()=>void
 }
