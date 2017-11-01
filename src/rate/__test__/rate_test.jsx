b1866481fab7a2245efde24428e2c79f508fc684
梁森 <sen.liang@ele.me>
2017-11-01 12:08:52 +0800

    fix some test bug

--- src/rate/__test__/rate_test.jsx
+++ src/rate/__test__/rate_test.jsx
@@ -17,14 +17,19 @@ test('Basic usage', () => {
 
   // 模拟component1的第二个星 mouseenter事件
   const rate1 = component1.find('.el-rate .el-rate__item').at(1);
-  rate1.simulate('mousemove');
-  expect(rate1.find('i').at(0).hasClass('hover')).toEqual(true);
+  rate1.simulate('mouseenter');
+  setTimeout(() => {
+    expect(rate1.find('i').at(0).hasClass('hover')).toEqual(true);
+  }, 1000)
 
   // 模拟component1的第三个星 click事件
   const rate2 = component1.find('.el-rate .el-rate__item').at(2);
   rate2.simulate('click');
-  expect(rate2.find('i').at(0).prop('style').color).toEqual('#F7BA2A');
-  expect(state1.value).toEqual(3);
+  setTimeout(() => {
+    expect(rate2.find('i').at(0).prop('style').color).toBe('#F7BA2A');
+    expect(state1.value).toEqual(3);
+  }, 1000)
+
 
   const component2 = mount(
     <Rate colors={['#99A9BF', '#F7BA2A', '#FF9900']} />
@@ -33,7 +38,9 @@ test('Basic usage', () => {
   // 模拟component2的第四个星 click事件
   const rate3 = component2.find('.el-rate .el-rate__item').at(3);
   rate3.simulate('click');
-  expect(rate3.find('i').at(0).prop('style').color).toEqual('#FF9900');
+  setTimeout(() => {
+    expect(rate3.find('i').at(0).prop('style').color).toEqual('#FF9900');
+  }, 1000)
 });
 
 test('With text', () => {
