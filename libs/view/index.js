cf93ed930887459e0d3f0d8fd93d86285a1c14e6
LoadChange <soocto@gmail.com>
2019-01-29 11:17:27 +0800

    fix: style 丢失

--- libs/view/index.js
+++ libs/view/index.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 
 export default class View extends Component {
   render() {
-    const mixed = { style: {} };
     const classNames = [];
     const { show = true, className = '', children } = this.props;
-    if (!show) mixed.style = { ...children.props.style, display: 'none' };
+    const mixed = { style: { ...children.props.style } };
+    if (!show) mixed.style.display = 'none';
     if (children.props.className) classNames.push(children.props.className);
     if (className) classNames.push(className);
     mixed.className = classNames.join(' ');
