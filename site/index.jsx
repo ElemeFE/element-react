dafefedfee7c18ee9c251c5ead60cb80d4511518
LoadChange <soocto@gmail.com>
2019-04-16 18:31:18 +0800

    refactor: add render callback

--- site/index.jsx
+++ site/index.jsx
@@ -11,16 +11,6 @@ import './styles/prism.css';
 
 import App from './page';
 
-render(<AppContainer><App /></AppContainer>, document.getElementById('app'));
-
-if (module.hot) {
-  module.hot.accept('./page', () => {
-    const App = require('./page').default;
-
-    render(<AppContainer><App /></AppContainer>, document.getElementById('app'));
-  });
-}
-
 function inChinaConfirm() {
   import('../src/message-box').then(MessageBox => {
     MessageBox.default.confirm('建议大陆用户访问部署在国内的站点，是否跳转？', '提示').then(() => {
@@ -41,4 +31,12 @@ function inChina() {
   }
 }
 
-setTimeout(() => inChina());
+render(<AppContainer><App /></AppContainer>, document.getElementById('app'), inChina);
+
+if (module.hot) {
+  module.hot.accept('./page', () => {
+    const App = require('./page').default;
+
+    render(<AppContainer><App /></AppContainer>, document.getElementById('app'));
+  });
+}
