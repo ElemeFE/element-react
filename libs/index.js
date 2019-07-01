57b115aef2d202b9f847617a9799455d594e1e5b
Emiya0306 <worfandsheep@gmail.com>
2019-07-01 11:39:11 +0800

    minor, fix scroll unlimited loop with PureComponent

--- libs/index.js
+++ libs/index.js
@@ -6,6 +6,7 @@
 export { default as CollapseTransition } from './collapse';
 export { default as Transition } from './transition';
 export { default as Component } from './component';
+export { default as PureComponent } from './pureComponent';
 export { default as PropTypes } from './props';
 export { default as View } from './view';
 export { default as Animate } from './animate';
